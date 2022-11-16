a17d7d3c9190a700e70f93e7f4c855d2bec93a17
Mark Csoma <84617390+midorisol@users.noreply.github.com>
2022-11-16 09:00:25 +0100

    Missed update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+
+export interface CovidRecord {
+  testAmount: number;
+  hospitalized: number;
+  date: Date;
+  infected?: number | undefined
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'week5home';
+  covidData: CovidRecord[] =[
+    {testAmount:1234, hospitalized:2500, date:new Date('2022-10-26')},
+    {testAmount:66423, hospitalized:2660, date:new Date('2022-10-27')},
+    {testAmount:43612, hospitalized:2760, date:new Date('2022-10-28')},
+    {testAmount:9744, hospitalized:2870, date:new Date('2022-10-29')},
+    {testAmount:531, hospitalized:2980, date:new Date('2022-10-30')},
+    {testAmount:9524, hospitalized:2770, date:new Date('2022-10-31')},
+    {testAmount:15313, hospitalized:2660, date:new Date('2022-11-01')},
+  ]
+  iWantTheTrust = false;
+
+  constructor() {
+    this.covidData.forEach(covidRecord => covidRecord.infected = this.generatedInfectedNumber(covidRecord))
+  }
+
+
+  private generatedInfectedNumber(covidRecord: CovidRecord){
+  let infectedNumber = Math.round(Math.random() * 500);
+  const infectedTreshold = Math.floor(covidRecord.testAmount * 0.05);
+
+  if(infectedNumber > infectedTreshold) {
+    infectedNumber = infectedTreshold;
+  }
+  return infectedNumber;
+  }
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(e: any) {
+      console.log(e)
+    if(e.code === 'KeyT' && e.altKey){
+      this.iWantTheTrust = !this.iWantTheTrust
+    }
+  }
 }
